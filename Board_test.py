4c9641a3c8cf29cc2c8fa6b228dd300f001af256
raynomd <rc.geem@gmail.com>
2020-01-03 09:39:12 -0500

    Started outlining the rest of the project.

--- Board_test.py
+++ Board_test.py
@@ -95,22 +95,3 @@ class TestBoard(unittest.TestCase):
         # Then, calculate the score by hand.
 
         #With the modifier, and without
-
-
-
-
-# create a board, which acts as a grid
-# each board square can hold one square, e.x. "4wheat",
-# These are permanent, immutable. Live on the board
-
-# Thus, a square can be "full" or "empty"
-
-# Empty squares should have the concept of "validity"
-# A check for validity happens as follows:
-# Poll two squares at once.
-# 1. Both must be empty (One of the tiles is already occupied!)
-# 2. Neither can be a "border square" - with an idx of 0 or "col_width/height (-1)"] (This move would exceed 5x5 grid)
-# 3. Check for adjacency - one of the adjacent tiles must be of the same "suit" or "wild"(No adjacent matching suit)
-
-# if it's all valid, prompt for confirmation. If yes, write it to the grid
-# if it's not valid, return a message for which validity requirement isn't met.
